5f76948321296e90790e074ca9753bf4aa0f0ff8
Guinpen07 <93430366+Guin07@users.noreply.github.com>
2021-11-11 16:26:36 +0500

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace По_заказу
 {
@@ -21,23 +21,48 @@ namespace По_заказу
         {
             //отображение данных
 
-            Console.WriteLine("Введите имя: "); //переменная, обозначающая имя
+            
+            int centerX = (Console.WindowWidth / 2);
+            int centerY = (Console.WindowHeight / 2);
+            Console.SetCursorPosition(centerX, centerY);
+            Console.WriteLine("Введите имя: ");   //переменная, обозначающая имя
             fullname = Console.ReadLine();
+            Console.Clear();
 
+            int centerX = (Console.WindowWidth / 2);
+            int centerY = (Console.WindowHeight / 2);
+            Console.SetCursorPosition(centerX, centerY);
             Console.WriteLine("Введите возраст: "); //переменная, обозначающая возраст
             age = byte.Parse(Console.ReadLine());
+            Console.Clear();
 
+            int centerX = (Console.WindowWidth / 2);
+            int centerY = (Console.WindowHeight / 2);
+            Console.SetCursorPosition(centerX, centerY);
             Console.WriteLine("Введите Ваш рост: "); //переменная, обозначающая рост
             height = double.Parse(Console.ReadLine());
+            Console.Clear();
 
+            int centerX = (Console.WindowWidth / 2);
+            int centerY = (Console.WindowHeight / 2);
+            Console.SetCursorPosition(centerX, centerY);
             Console.WriteLine("Введите Ваши баллы по русскому языку: "); //переменная, обозначающая баллы за русский язык
             russianlanguagescores = double.Parse(Console.ReadLine());
+            Console.Clear();
 
+            int centerX = (Console.WindowWidth / 2);
+            int centerY = (Console.WindowHeight / 2);
+            Console.SetCursorPosition(centerX, centerY);
             Console.WriteLine("Введите Ваши баллы по математике: "); //переменная, обозначающая баллы за математику
             mathscores = double.Parse(Console.ReadLine());
+            Console.Clear();
 
+            int centerX = (Console.WindowWidth / 2);
+            int centerY = (Console.WindowHeight / 2);
+            Console.SetCursorPosition(centerX, centerY);
             Console.WriteLine("Введите Ваши баллы по истории: "); //переменная, обозначающая баллы за историю
             historyscores = double.Parse(Console.ReadLine());
+            Console.Clear();
 
             double valueA = russianlanguagescores; double valueB = mathscores; double valueC = historyscores; //расчёт среднего арифметического из общего количества баллов
             double valueD = valueA + valueB + valueC;
@@ -46,9 +71,16 @@ namespace По_заказу
 
             string newpattern_2 = "{0} \n"; //вывод ответа расчёта 
 
+            int centerX = (Console.WindowWidth / 2);
+            int centerY = (Console.WindowHeight / 2);
+            Console.SetCursorPosition(centerX, centerY);
             Console.WriteLine("Посчитать среднее арифметическое Ваших баллов? "); //переменная, обозначающая ответ за среднее арифметическое
             answer = Console.ReadLine();
+            Console.Clear();
 
+            int centerX = (Console.WindowWidth / 2);
+            int centerY = (Console.WindowHeight / 2);
+            Console.SetCursorPosition(centerX, centerY);
             Console.WriteLine(newpattern_2, //сам ответ
                 valueF);
 
