c1f42e1aa9e3a091f9c354b10e9445f4a5a0784d
Guinpen07 <93430366+Guin07@users.noreply.github.com>
2021-11-25 01:06:27 +0500

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -8,15 +8,7 @@ using System.Threading.Tasks;
 namespace _3._2
 {
     class Program
-    {
-        private static int sum;
-       
-        public static int T { get; private set; }
-        public static int K { get; private set; }
-        public static int Q { get; private set; }
-        public static int J { get; private set; }
-       
-        
+    {       
         static void Main(string[] args)
         {
             Console.WriteLine("Здравствуйте, игрок. Сколько у Вас карт на руках?");
@@ -33,43 +25,43 @@ namespace _3._2
                 switch (card)
                 {
                     case "T":
-                        sum += 10;
+                        summ += 10;
                         break;
                     case "K":
-                        sum += 10;
+                        summ += 10;
                         break;
                     case "Q":
-                        sum += 10;
+                        summ += 10;
                         break;
                     case "J":
-                        sum += 10;
+                        summ += 10;
                         break;
                     case "10":
-                        sum += 10;
+                        summ += 10;
                         break;
                     case "9":
-                        sum += 9;
+                        summ += 9;
                         break;
                     case "8":
-                        sum += 8;
+                        summ += 8;
                         break;
                     case "7":
-                        sum += 7;
+                        summ += 7;
                         break;
                     case "6":
-                        sum += 6;
+                        summ += 6;
                         break;
                     case "5":
-                        sum += 5;
+                        summ += 5;
                         break;
                     case "4":
-                        sum += 4;
+                        summ += 4;
                         break;
                     case "3":
-                        sum += 3;
+                        summ += 3;
                         break;
                     case "2":
-                        sum += 2;
+                        summ += 2;
                         break;
                     default:
                         Console.WriteLine("Таких карт не существует");
